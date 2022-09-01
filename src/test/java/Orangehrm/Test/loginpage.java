af0830d4ad317974c77d0742295f8175bb55deba
RemanSharma12 <reman.sharma@infostride.com>
2022-09-01 22:32:50 +0530

    orangehrm

--- src/test/java/Orangehrm/Test/loginpage.java
+++ src/test/java/Orangehrm/Test/loginpage.java
@@ -4,6 +4,6 @@ public class loginpage extends baseclass{
     public static void main(String args[]) throws InterruptedException{
         setup();
         pagefactory.getLogin().login();
-
+        pagefactory.getLogin().admin();
     }
 }
--- src/test/java/Orangehrm/pages/login.java
+++ src/test/java/Orangehrm/pages/login.java
@@ -2,6 +2,7 @@ package Orangehrm.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class login {
     WebDriver driver;
@@ -23,7 +24,12 @@ public class login {
         driver.findElement(submit).click();
     }
     public  void admin(){
-        driver.findElement(By.xpath("//span[contains(text(),'Admin')]"));
+        driver.findElement(By.partialLinkText("Admin")).click();
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
+       driver.findElement(By.xpath("//div[@class='oxd-select-text--after']")).click();
+       driver.findElement(By.xpath("//div[contains(text(),'Admin')]"));
+//        Select objSelect =new Select(driver.findElement(By.id("(//div[@class='oxd-select-text oxd-select-text--active'])[1]")));
+//        objSelect.selectByVisibleText("Admin");
     }
 
 }
